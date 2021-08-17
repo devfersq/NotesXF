cf94fe13f841e2ca78fbb5b7db84b4475cca654d
Fernando Sanchez Q <dev.fersq@outlook.com>
2021-08-17 15:43:10 -0600

    En este commit se agrega la solucion del proyecto con la funcionalidad de guardar una nota.

---
+++ XamarinNotesApp/XamarinNotesApp/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinNotesApp
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            notesListView.Refreshing += NotesListView_Refreshing;
+		}
+
+        private void NotesListView_Refreshing(object sender, EventArgs e)
+        {
+            ReadPosts();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            ReadPosts();
+        }
+
+        private void NewToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new NewNotePage());
+        }
+
+        private void NotesListView_ItemSelected(object sender, EventArgs e)
+        {
+            Note selectedNote = notesListView.SelectedItem as Note;
+            Navigation.PushAsync(new NewNotePage(selectedNote));
+        }
+
+        private void MenuItem_Delete(object sender, EventArgs args)
+        {
+            Note itemToDelete = ((sender as MenuItem).BindingContext as Note);
+
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabasePath))
+            {
+                conn.CreateTable<Note>();
+                conn.Delete(itemToDelete);
+            }
+
+            ReadPosts();
+        }
+
+        private void ReadPosts()
+        {
+            //! added using SQLite;
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabasePath))
+            {
+                conn.CreateTable<Note>();
+                List<Note> notes = conn.Table<Note>().ToList();
+                notesListView.ItemsSource = notes;
+                notesListView.IsRefreshing = false;
+            }
+        }
+    }
+}
---
+++ XamarinNotesApp/XamarinNotesApp/NewNotePage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinNotesApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NewNotePage : ContentPage
+	{
+        Note selectedNote;
+        public NewNotePage()
+        {
+            InitializeComponent();
+        }
+		public NewNotePage (Note selectedNote)
+		{
+			InitializeComponent ();
+
+            this.selectedNote = selectedNote;
+            titleEntry.Text = selectedNote.Title;
+            contentEditor.Text = selectedNote.Content;
+		}
+
+        private void SaveToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            if (selectedNote == null)
+            {
+                Note note = new Note()
+                {
+                    Title = titleEntry.Text,
+                    Content = contentEditor.Text
+                };
+
+                //! added using SQLite;
+                using (SQLiteConnection conn = new SQLiteConnection(App.DatabasePath))
+                {
+                    conn.CreateTable<Note>();
+                    int itemsInserted = conn.Insert(note);
+
+                    if (itemsInserted > 0)
+                        DisplayAlert("Done", "Note saved", "Ok");
+                    else
+                        DisplayAlert("Error", "Note not saved", "Ok");
+                }
+            }
+            else
+            {
+                selectedNote.Title = titleEntry.Text;
+                selectedNote.Content = contentEditor.Text;
+
+                using (SQLiteConnection conn = new SQLiteConnection(App.DatabasePath))
+                {
+                    conn.CreateTable<Note>();
+                    int itemsUpdated = conn.Insert(selectedNote);
+
+                    if (itemsUpdated > 0)
+                        DisplayAlert("Done", "Note updated", "Ok");
+                    else
+                        DisplayAlert("Error", "Note not updated", "Ok");
+                }
+            }
+        }
+    }
+}
